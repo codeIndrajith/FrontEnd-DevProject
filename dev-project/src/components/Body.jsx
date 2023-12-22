c71ee4f531ef1297b832205a179bdb457e637709
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-12-23 00:28:24 +0530

    Final Responsive style

--- dev-project/src/components/Body.jsx
+++ dev-project/src/components/Body.jsx
@@ -25,7 +25,8 @@ function Body() {
         </div>
       </div>
 
-      <div className="md:w-[1064px] md:h-[414px] sm:w-[335px] sm:h-[572.82px] flex flex-col lg:justify-center items-center sm:justify-center sm:flex-col md:flex-row lg:flex-row lg:items-start">
+      {/* Display to large screen */}
+      <div className="hidden md:flex md:w-[1064px] md:h-[414px] sm:w-[335px] sm:h-[572.82px] flex flex-col lg:justify-center items-center sm:justify-center sm:flex-col md:flex-row lg:flex-row lg:items-start">
         <div className="space-y-4 sm:space-y-8 lg:w-[542px] lg:h-[206px] sm:w-[393px] flex flex-col justify-center lg:items-start md:items-start sm:items-center items-center">
           <p className="text-[#6B3CC9] text-[27px] font-semibold">
             Digital Strategy Consulting
@@ -44,6 +45,28 @@ function Body() {
 
         <div className="lg:w-[414px] lg:h-[416px] sm:w-[275px] sm:h-[276px]">
           <img src={searchImg} alt="icon" />
+        </div>
+      </div>
+
+      {/* Display to small screen */}
+      <div className="md:hidden md:w-[1064px] md:h-[414px] sm:w-[335px] sm:h-[572.82px] flex flex-col lg:justify-center items-center sm:justify-center sm:flex-col md:flex-row lg:flex-row lg:items-start">
+        <div className="space-y-4 sm:space-y-8 lg:w-[542px] lg:h-[206px] sm:w-[393px] flex flex-col justify-center lg:items-start md:items-start sm:items-center items-center">
+          <div className="lg:w-[414px] lg:h-[416px] sm:w-[275px] sm:h-[276px]">
+            <img src={searchImg} alt="icon" />
+          </div>
+          <p className="text-[#6B3CC9] text-[27px] font-semibold">
+            Digital Strategy Consulting
+          </p>
+          <p className="font-normal">
+            Your digital strategy should complement the overall marketing
+            strategy of the company. In online marketing, each component will
+            never work in isolation and every business needs a different mix. We
+            provide a clear concept and strategic overview to find the most
+            efficient model for your business.
+          </p>
+          <button className="w-[129px] h-[38px] bg-[#F28D35] flex justify-center items-center text-white rounded text-[14px] font-bold">
+            LEARN MORE
+          </button>
         </div>
       </div>
     </div>
